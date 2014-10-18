a19c6a9b10a949a062aeda1dc94f8de9082713d9
Kenneth Lewis <lewisenator@gmail.com>
2014-10-18 13:07:42 -0700

    allow submission of name parameter

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :name) }
   end
 end
