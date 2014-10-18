e149fc531a4dbe38bdea42fb8d4ce72690fab33a
Kenneth Lewis <lewisenator@gmail.com>
2014-10-18 15:52:30 -0700

    stubbed out sections

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,19 @@
 class DashboardController < ApplicationController
 	before_action :authenticate_user!
-	
+
 	def index
+    @sections = [{
+      image_name: 'clapper_perspective.png',
+      name: 'Movies',
+      url: movies_path
+    }, {
+      image_name: 'book_coming_soon.png',
+      name: 'Books',
+      url: '#'
+    }, {
+      image_name: 'game_coming_soon.png',
+      name: 'Video Games',
+      url: '#'
+    }].map{|x| OpenStruct.new(x)}
 	end
 end
