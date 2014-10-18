d350670caa471edc3c4301f39ff22211eea1ab5f
Kenneth Lewis <lewisenator@gmail.com>
2014-10-17 23:12:50 -0700

    callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,34 +33,43 @@ class User < ActiveRecord::Base
 
   validates_format_of :email, without: TEMP_EMAIL_REGEX, on: :update
 
-  def self.find_for_oauth(auth, signed_in_resource = nil)
+  def self.from_omniauth(auth)
+	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	    user.email = auth.info.email
+	    user.password = Devise.friendly_token[0,20]
+	    user.name = auth.info.name   # assuming the user model has a name
+	    user.image = auth.info.image # assuming the user model has an image
+	  end
+	end
 
-    identity = Identity.find_for_oauth(auth)
+  # def self.find_for_oauth(auth, signed_in_resource = nil)
 
-    user = signed_in_resource ? signed_in_resource : identity.user
+  #   identity = Identity.find_for_oauth(auth)
 
-    if user.nil?
-      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
-      email = auth.info.email if email_is_verified
-      user = User.where(:email => email).first if email
+  #   user = signed_in_resource ? signed_in_resource : identity.user
 
-      if user.nil?
-        user = User.new(
-          name: auth.extra.raw_info.name,
-          email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
-          password: Devise.friendly_token[0,20]
-        )
-        user.skip_confirmation!
-        user.save!
-      end
-    end
+  #   if user.nil?
+  #     email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+  #     email = auth.info.email if email_is_verified
+  #     user = User.where(:email => email).first if email
 
-    if identity.user != user
-      identity.user = user
-      identity.save!
-    end
-    user
-  end
+  #     if user.nil?
+  #       user = User.new(
+  #         name: auth.extra.raw_info.name,
+  #         email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+  #         password: Devise.friendly_token[0,20]
+  #       )
+  #       user.skip_confirmation!
+  #       user.save!
+  #     end
+  #   end
+
+  #   if identity.user != user
+  #     identity.user = user
+  #     identity.save!
+  #   end
+  #   user
+  # end
 
   def email_verified?
     self.email && self.email !~ TEMP_EMAIL_REGEX
